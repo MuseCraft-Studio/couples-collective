71a07594c6b77676ec63ab4f909078b5262ad466
MuseCraft Studio <themusecrafter@gmail.com>
2023-08-03 15:09:17 -0400

    ninja update again

--- contact.php
+++ contact.php
@@ -3,9 +3,31 @@
 Template Name: contact
 */
 ?>
-<div>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Contact">
+    <?php 
+        wp_head();
+    ?>
+</head>
+<body data-page="contact">
 <?php
-the_content()
+    get_header()
 ?>
-</div>
-<? php wp_footer (); ?>
+
+<?php
+    the_content()
+?>
+
+
+<?php
+    get_footer()
+?>
+</body>
