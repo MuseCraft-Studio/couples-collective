f1682e13dc29c657a9cfbeaac1cd4e73e989230d
MuseCraft Studio <themusecrafter@gmail.com>
2023-08-16 13:00:45 -0400

    split out services, updated css

---
+++ couples.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Template Name: couples
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Couples Collective - Couples Service</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Couples Service">
+    <?php 
+        wp_head();
+    ?>
+</head>
+
+
+<body data-page="couples">
+    <!-- <section id="media-level"></section> -->
+    <div id="standard-header">
+        <?php
+            get_header()
+        ?>
+    </div>
+    <diu class="services">
+        <div id="services-two">
+            <div class="flex-container">
+                <div class="flex-item">
+                    <div class="image"></div>
+                </div>
+                <div class="flex-item">
+                    <div class="title">
+                        Tell us about you and your partner and we can find the right match for you as a couple.
+                    </div>
+                    <div class="content">
+                        <p>Do you feel lonely, unacknowledged, or stuck in your partnership? When you try to communicate, does it seem like you’re talking to a wall—as if your feelings don't really matter? Do you ever think you are not good enough—that you can never catch a break or get it right?</p>
+                        <p>Perhaps you lack depth in your relationship and want to break down emotional barriers and enhance your physical connection. Conversely, maybe an act of infidelity has fractured your trust, and you want to know if there is still any hope for your marriage. As a relationship evolves, it's natural for couples to run into unexpected challenges. For instance, you may be having trouble coordinating your communication or parenting styles in a blended family dynamic. Or your partner may have put up emotional barriers, and you don't know how to make a connection without feeling criticized or misunderstood for trying. And if you are recovering from infidelity, it can be extremely difficult managing the intense mix of emotions in a way that promotes internal healing.</p>
+                    </div>
+                    <a class="fill-button" href="https://forms.gle/PwZLNmbNeMNwkArAA" target="_blank">Contact us</a>
+                </div>
+            </div>
+        </div>
+        <div id="services-three">
+            <div class="call-now">
+                Or call now  (510) 373-9995
+            </div>
+        </div>
+    </div>
+    <div class="standard-footer">
+        <?php
+            get_footer()
+        ?>
+    </div>
+</body>
+
+</html>
---
+++ individuals.php
@@ -0,0 +1,106 @@
+<?php
+/*
+Template Name: individuals
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Couples Collective - Services</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Services">
+    <?php 
+        wp_head();
+    ?>
+</head>
+
+
+<body data-page="services">
+    <!-- <section id="media-level"></section> -->
+    <div id="standard-header">
+        <?php
+            get_header()
+        ?>
+    </div>
+    <diu class="services">
+        <div id="services-one">
+            <div class="container">
+                <div class="text-content">
+                    <div class="title">
+                        Providing expert EFT relationship counseling for individuals
+                    </div>
+                    <div class="content">
+                        <p>We work with individuals to support each person's growth and life goals. In individual therapy, we use the EFT model to work together to build a better understanding of relationship and personal habitual patterns that no longer serve you and your aspirations to having relationships that you want.</p>
+                        <p style="font-family: var(--font-work-sans-bold)">Is therapy working for you?</p>
+                        <p>
+                            One of the highest indicators for success in therapy is the rapport and relationship with your therapist. To determine if we would be a good fit, our relationship would start with a no-cost 20 minute phone consultation to determine if my style and areas of focus are aligned with what you are looking to address in your life.
+                        </p>
+                    </div>
+                </div>
+                <div class="get-started">
+                    <a class="fill-button" href="https://forms.gle/PwZLNmbNeMNwkArAA" target="_blank">Get started</a>
+                </div>
+            </div>
+        </div>
+        <div id="services-four">
+            <div class="container">
+                <div class="flex-item">
+                    <div class="title">
+                        Fees and Sessions
+                    </div>
+                    <div class="description">
+                        <p>
+                        Clients are expected to pay for each session at the time it is held or, if you prefer, you may pay in advance. Please contact us to discuss fees as they vary based on each therapist.
+                        </p>
+                        <p>
+                        We recommend an initial couples counseling session of 80 minutes. Subsequent sessions typically run 55 - 60 mins for couples, but can be adjusted to 80 upon request.
+                        </p>
+                    </div>
+                </div>
+                <div class="flex-item">
+                    <div class="title">
+                    Payment methods
+                    </div>
+                    <div class="description">
+                        <p>
+                        Cash, check, HSA or FSA and credit cards are accepted. The Couples Collective does not take insurance directly, we do however provide a monthly superbill receipt of services rendered that can be submitted for reimbursement to your insurance carrier. 
+                        </p>
+                        <p>
+                        Reduced fees are available upon approval depending on therapists availability and ability to accommodate.
+                        </p>
+                    </div>
+                </div>
+                <div class="flex-item">
+                    <div class="title">
+                    Policies
+                    </div>
+                    <div class="description">
+                        <p>
+                        Canceling of appointments must be done 24 hours in advance, or a full session charge is due.
+                    </p>
+                    <p>
+                        Legal and ethical responsibilities require private sessions remain confidential, no information will be released to others without written consent. Exceptions include, threat of endangering yourself, others or in the event of child or elder abuse.
+                    </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="services-three">
+            <div class="call-now">
+                Or call now  (510) 373-9995
+            </div>
+        </div>
+    </div>
+    <div class="standard-footer">
+        <?php
+            get_footer()
+        ?>
+    </div>
+</body>
+
+</html>
