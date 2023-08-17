a81020c472679d5e4811244c9c1d0751df28d9ad
MuseCraft Studio <themusecrafter@gmail.com>
2023-08-17 13:16:39 -0400

    404 updated

--- 404.php
+++ 404.php
@@ -1,3 +1,23 @@
-<div>
-    404, not found!
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Couples Collective - Adam Harrison, LCSW</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Adam Harrison, LCSW">
+    <?php 
+    wp_head();
+    ?>
+</head>
+<body>
+    <div class="four-oh-four">
+        Uh oh, page not found!
+        <p>
+            Click <a href="/">here </a>to return to the home page
+        </p>
+    </div>
+</body>
+</html>
--- header.php
+++ header.php
@@ -30,16 +30,16 @@
                     </div>
                     <div id="myLinks">
                         <div class="text-links">
-                        <?php
-                            wp_nav_menu(
-                                array(
-                                    'menu' => 'primary',
-                                    'container' => '',
-                                    'theme_location' => 'primary',
-                                    'items_wrap' => '%3$s'
-                                )
-                            );
-                        ?>
+                            <?php
+                                wp_nav_menu(
+                                    array(
+                                        'menu' => 'primary',
+                                        'container' => '',
+                                        'theme_location' => 'primary',
+                                        'items_wrap' => '%3$s'
+                                    )
+                                );
+                            ?>
                         </div>
                         <div class="button-link">
                             <a class="fill-button" href="tel:5103739995">Call Now</a>
