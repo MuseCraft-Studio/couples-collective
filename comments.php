ec56e83fa4747931ab6e71a8253d8d5239ee6512
MuseCraft Studio <themusecrafter@gmail.com>
2023-08-24 16:03:14 -0400

    comments updated

--- comments.php
+++ comments.php
@@ -1,28 +1,40 @@
 <?php
-    if (!have_comments()) {
-        echo "Leave a comment";
-    } else {
-        echo get_comments_number(). " Comments";
-    }
-?>
-
-<?php
-    wp_list_comments(
-        array (
-            'avatar_size' => 120,
-            'style' => 'div', 
-        )
-    );
-?>
-
-<?php
     if (comments_open() ) {
         comment_form(
             array(
                 'class_form' => '',
-                'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
-                'title_reply_after' => '</h2>',
+                'title_reply' => '',
+                'title_reply_before' => '<p>',
+                'title_reply_after' => '</p>',
+                'label_submit' => 'Post',
+                'comment_field' => '<textarea id="comment-area" name="comment" placeholder="Leave a comment..." rows=10 required="required"></textarea>',
             )
         );
     }
 ?>
+
+<div id="comments-title">
+    <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/comment.png" alt="comments icon"/></div>
+    <div>comments </div>
+    <div id="comment-number">
+    <?php
+        if (have_comments()) {
+            echo get_comments_number();
+        } else {
+            echo '0';
+        }
+    ?>
+    </div>
+</div>
+
+<div id="comment-list">
+    <?php
+        wp_list_comments(
+            array (
+                'avatar_size' => 65,
+                'style' => 'div', 
+            )
+        );
+    ?>
+</div>
+
