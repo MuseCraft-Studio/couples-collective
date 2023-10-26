ca7dc80031cf6053a548e2125fc2376a952aa51e
MuseCraft Studio <themusecrafter@gmail.com>
2023-10-26 14:09:02 -0400

    created lgbtq page

---
+++ lgbtq.php
@@ -0,0 +1,196 @@
+<?php
+/*
+Template Name: lgbtq
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Couples Collective - LGBTQ EFT Individual and Couples Counseling</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="LGBTQ EFT Individual and Couples Counseling">
+    <?php 
+        wp_head();
+    ?>
+</head>
+
+
+<body data-page="sex-intimacy">
+    <!-- <section id="media-level"></section> -->
+    <div id="standard-header">
+        <?php
+            get_header()
+        ?>
+    </div>
+    <div class="services">
+        <div id="services-five">
+            <div class="desktop">
+                <div class="flex-container">
+                    <div class="flex-item">
+                        <div class="image"></div>
+                    </div>
+                    <div class="flex-item">
+                        <div class="title">
+                            LGBTQ EFT Individual and Couples Counseling
+                        </div>
+                        <div class="content">
+                            <p>Emotionally Focused Couples Therapy (EFT) has emerged as a highly effective approach for LGBTQ couples and individuals seeking counseling. Emotionally Focused Couples Therapy (EFT) is a therapeutic approach designed to help couples build and strengthen emotional connections. It's based on the idea that a secure emotional bond is essential for a thriving relationship, regardless of sexual orientation or gender identity. EFT focuses on creating a safe space for partners to express their feelings, needs, and fears openly. This approach has proven to be particularly effective for LGBTQ couples.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mobile">
+                <div class="flex-container">
+                    <div class="flex-item">
+                        <div class="title">
+                            LGBTQ EFT Individual and Couples Counseling
+                        </div>
+                        <p>Emotionally Focused Couples Therapy (EFT) has emerged as a highly effective approach for LGBTQ couples and individuals seeking counseling. Emotionally Focused Couples Therapy (EFT) is a therapeutic approach designed to help couples build and strengthen emotional connections. It's based on the idea that a secure emotional bond is essential for a thriving relationship, regardless of sexual orientation or gender identity. EFT focuses on creating a safe space for partners to express their feelings, needs, and fears openly. This approach has proven to be particularly effective for LGBTQ couples.</p>
+                    </div>
+                    <div class="flex-item">
+                        <div class="image"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="desktop-subtitle">
+                <b>Why EFT Works for LGBTQ Couples</b>
+            </div>
+            <div class="flex-container-two">
+                <div class="flex-item">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Stocksy_txp2c7f6af1x1k200_Small_1173368.jpg" alt="Two men in love embrace tenderly" />
+                    <div class="small-title">Respect for Diversity</div>
+                    <div>
+                        Our EFT therapists are trained to understand and respect the unique experiences of LGBTQ individuals and couples. They provide a safe, affirming environment where clients can discuss issues related to their sexual orientation or gender identity without fear of judgment.                    </div>
+                    </div>
+                <div class="flex-item">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Stocksy_txp2c7f6af1x1k200_Small_2535983.jpg" alt="Young women kissing" />
+                    <div class="small-title">Addressing Unique Challenges</div>
+                    <div>
+                        EFT therapists recognize that LGBTQ couples may face distinct challenges, such as coming out, family acceptance, and societal prejudice. EFT helps couples navigate these challenges by fostering better communication and emotional support.
+                    </div>
+                </div>
+                <div class="flex-item">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Stocksy_txp181f7a45MqN200_Medium_1710039.jpg" alt="Smiling couple at wall" />
+                    <div class="small-title">Promoting Attachment</div>
+                    <div>
+                        EFT emphasizes the importance of attachment bonds in relationships. For LGBTQ couples, especially those in the Bay Area, where community support is essential, EFT can help strengthen these connections and build a stronger sense of belonging.
+                    </div>
+                </div>
+                <div class="flex-item">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Stocksy_txpd43da0b5eUQ000_Medium_273418.jpg" alt="Image of group of friends hang out in the city" />
+                    <div class="small-title">Effective Communication</div>
+                    <div>
+                        EFT equips couples with the skills to communicate their needs and emotions effectively. This is particularly valuable for LGBTQ couples who may struggle to express their feelings openly due to societal pressures or past traumas.
+                    </div>
+                </div>
+            </div>      
+        </div>
+        <div id="services-six">
+            <div class="flex-container">
+                <div class="fill-width">
+                    <div class="title">
+                    Specialized EFT for LGBTQ Couples In the Bay Area, where diversity is celebrated, EFT therapists often offer specialized LGBTQ couples therapy
+                    </div>
+                </div>
+                <div class="issues-container">
+                    <div class="flex-item">
+                        <div><b>Gay Couples Therapy</b></div>
+                        <div>
+                        Tailored to the unique experiences of gay couples, this form of EFT helps address issues related to identity, intimacy, and relationship dynamics.
+                        </div>
+                    </div>    
+                    <div class="flex-item">
+                        <div><b>Trans Couples Therapy</b></div>
+                        <div>
+                            EFT for transgender couples focuses on supporting the emotional journey of transitioning individuals and their partners while strengthening their connection.
+                        </div>
+                    </div>    
+                    <div class="flex-item">
+                        <div><b>Lesbian Couples Therapy</b></div>
+                        <div>
+                            EFT for lesbian couples addresses issues specific to women-loving-women relationships, promoting emotional intimacy and resilience.
+                        </div>
+                    </div>        
+                </div>
+                <div class="fill-width">
+                    <p>
+                        With a strong emphasis on emotional connection, open communication, and respect for diversity, our EFT therapists provide a safe haven for couples to thrive. Whether you are a gay, trans, or lesbian couple, EFT can help you strengthen your bond and navigate the unique challenges you may face.
+                    </p>
+                    <p>
+                        If you're seeking LGBTQ EFT counseling in the Bay Area, we invite you to connect with our experienced therapists who are dedicated to helping you build a healthier, more fulfilling relationship. Contact us today to take the first step toward a brighter future together.
+                    </p>
+                </div>
+                <div class="fill-width-center">
+                    <a class="fill-button" href="/contact" target="_blank">Contact us</a>
+                </div>
+            </div>
+        </div>
+        <div id="services-four">
+            <div class="container">
+                <div class="flex-item">
+                    <div class="svg-text-container">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Clock.svg" alt="clock"/>
+                        <div class="title">
+                            Fees and Sessions
+                        </div>
+                        <div class="description">
+                            <p>
+                                Clients are expected to pay for each session at the time it is held or, if you prefer, you may pay in advance. Please contact us to discuss fees as they vary based on each therapist.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex-item">
+                    <div class="svg-text-container">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Card-Big.svg" alt="card-big"/>
+                        <div class="title">
+                            Payment methods
+                        </div>
+                        <div class="description">
+                            <p>
+                                Cash, check, HSA or FSA and credit cards are accepted. The Couples Collective does not accept insurance directly. We do, however, provide a monthly superbill of services rendered that may be submitted for reimbursement to your insurance carrier.
+                            </p>
+                            <p>
+                                Reduced fees are available upon approval depending on clinician availability and ability to accommodate.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex-item">
+                    <div class="svg-text-container">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Clipboard.svg" alt="clipboard"/>
+                        <div class="title">
+                            Policies
+                        </div>
+                        <div class="description">
+                            <p>
+                                Our practice requires a 24-hour notice for all cancellations. The full session fee will apply if a timely cancellation is not provided.
+                            </p>
+                            <p>
+                                Legal and ethical responsibilities require that private sessions remain confidential. Information will not be released to others without written consent. Exceptions include: threat of endangering yourself, others or in the event of child or elder abuse.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="services-three">
+            <div class="call-now">
+                Or call now  (510) 373-9995
+            </div>
+        </div>
+    </div>
+    <div class="standard-footer">
+        <?php
+            get_footer()
+        ?>
+    </div>
+    <script type="text/javascript" src="//cdn.callrail.com/companies/636674144/9a8ff257e0969e4278e2/12/swap.js"></script>
+</body>
+
+</html>
