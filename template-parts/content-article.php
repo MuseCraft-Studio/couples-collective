03bb84cc0b91a5f549593f726aa0925e33218a0d
MuseCraft Studio <themusecrafter@gmail.com>
2023-11-28 15:38:13 -0500

    removed tags from blog post

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -1,7 +1,6 @@
 
 <div>
-    <span><?php the_date() ?></span>
-    <span><?php the_tags('<span class="tag"><i class="fa fa-tag"></i>', '</span><span class="tag"><i class="fa fa-tag"></i>', '</span>'); ?></span>
+    <span style="margin-right: 16px;"><?php the_date() ?></span>
     <span><?php comments_number() ?></span>
     <?php
         the_content();
