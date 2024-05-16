abf790606d0427af06eba1a5970e7571165f8dfd
MuseCraft Studio <themusecrafter@gmail.com>
2024-05-16 01:39:53 -0400

    work with us css updated
    not my most inspired design...

--- careers.php
+++ careers.php
@@ -31,22 +31,32 @@ Template Name: careers
     </div>
     <div id="full-page">
         <div id="work-with-us">
-            <h1>Work With Us</h1>
-            <h2>Information about our practice</h2>
-            <p>Our practice offers in person and telehealth sessions to clients residing in California. Our two office locations make it amenable for clients to be able to attend sessions in person in San Francisco and the Bay Area.</p>
-            <h2>Requirements for our candidates</h2>
-            <ul>
-                <li>As we primarily utilize Emotionally Focused Therapy (EFT), we require our associates and licensed clinicians to have completed an EFT externship before working with our practice. An interest in making EFT the focus of couples work is also essential.</li>
-                <li>The clinician must be an associate or licensed clinician in the state of California in good standing with their board.</li>
-                <li>The clinician must hold their own personal liability insurance.</li>
-                <li>Associates and Licensed clinicians are expected to hold availability for client sessions for at least one evening per week, and to be available for in-person sessions at our East Bay Oakland location at least two days per week. Hybrid options are available outside of those limitations.</li>
-                <li>The clinician must be available to hold a caseload of at least 10 couples.</li>
-            </ul>
-            <h2>Other information</h2>
-            <ul>
-                <li>Associate clinicians will be offered weekly supervision consisting of triadic sessions, with individual check-ins as needed.</li>
-                <li>Licensed clinicians will have individual check-ins as needed.</li>
-            </ul>
+            <div class="about">
+                <div class="title">About our practice</div>
+                <p>Our practice offers in person and telehealth sessions to clients residing in California. Our two office locations make it amenable for clients to be able to attend sessions in person in San Francisco and the Bay Area.</p>
+            </div>
+            <div class="flex-container-left">
+                <div class="flex-item">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/careers/rock.jpg" alt="woman holding heart shaped rock"/>
+                </div>
+                <div class="flex-item">
+                    <div class="title">Requirements for our candidates</div>
+                    <ul>
+                        <li>As we primarily utilize Emotionally Focused Therapy (EFT), we require our associates and licensed clinicians to have completed an EFT externship before working with our practice. An interest in making EFT the focus of couples work is also essential.</li>
+                        <li>The clinician must be an associate or licensed clinician in the state of California in good standing with their board.</li>
+                        <li>The clinician must hold their own personal liability insurance.</li>
+                        <li>Associates and Licensed clinicians are expected to hold availability for client sessions for at least one evening per week, and to be available for in-person sessions at our East Bay Oakland location at least two days per week. Hybrid options are available outside of those limitations.</li>
+                        <li>The clinician must be available to hold a caseload of at least 10 couples.</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="about">
+                <div class="title">Other information</div>
+                <ul>
+                    <li>Associate clinicians will be offered weekly supervision consisting of triadic sessions, with individual check-ins as needed.</li>
+                    <li>Licensed clinicians will have individual check-ins as needed.</li>
+                </ul>
+            </div>
             <div>
                 <p>If you're interested in applying for a position with The Couples Collective, we encourage you to complete our form as the beginning of the application process. Our intake coordinator will respond to you within 2-3 business days after your form has been submitted.</p>
                 <div class="interest-form">
